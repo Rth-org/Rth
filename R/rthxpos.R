884b665af705f2f116b817d3bceb829d8774a486
wrathematics <wrathematics@gmail.com>
2014-06-03 09:16:23 -0400

    avoid extra copy in transpose

--- R/rthxpos.R
+++ R/rthxpos.R
@@ -1,7 +1,10 @@
 
 rthxpos <- function(m) {
    tmp <- .Call("rthxpos",m,PACKAGE="Rth")
-   matrix(tmp,nrow=ncol(m))
+#   matrix(tmp,nrow=ncol(m))
+   dim(tmp) <- c(ncol(m), nrow(m))
+   
+   return(tmp)
 }
 
 
