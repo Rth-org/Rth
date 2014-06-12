20674e061311eaf4d7cd416a5662070ee33f0ee1
Drew Schmidt <schmidt@math.utk.edu>
2014-06-12 06:49:01 -0400

    more robust backend.h

--- src/backend.h
+++ src/backend.h
@@ -8,20 +8,30 @@
 
 // FIXME should we include CPP ?
 
-#if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_OMP
-#include <omp.h>
-#define RTH_OMP 1
+#if THRUST_VERSION >= 100700
+  #if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_OMP
+  #include <omp.h>
+  #define RTH_OMP 1
 
-#elif THRUST_DEVICE_SYSTEM == THRUST_DEVICE_BACKEND_TBB
-#include "tbb/task_scheduler_init.h"
-#define RTH_TBB 1
+  #elif THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_TBB
+  #include "tbb/task_scheduler_init.h"
+  #define RTH_TBB 1
 
-#elif THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
-#define RTH_CUDA 1
-// TODO ???
+  #elif THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
+  #define RTH_CUDA 1
+  #endif
+#else
+  #if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_BACKEND_OMP
+  #include <omp.h>
+  #define RTH_OMP 1
 
+  #elif THRUST_DEVICE_SYSTEM == THRUST_DEVICE_BACKEND_TBB
+  #include "tbb/task_scheduler_init.h"
+  #define RTH_TBB 1
+
+  #elif THRUST_DEVICE_SYSTEM == THRUST_DEVICE_BACKEND_CUDA
+  #define RTH_CUDA 1
+  #endif
 #endif
-
-
 
 #endif
