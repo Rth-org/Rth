5c3cd75dc68fafa57aae448895cdefc7443fa56b
matloff <matloff@cs.ucdavis.edu>
2014-06-15 20:47:13 -0700

    fixed bug, re check of m instead of m1,m2

--- R/rthpdist.R
+++ R/rthpdist.R
@@ -3,7 +3,8 @@ rthpdist <- function(m1,m2, nthreads=rth.nthreads()) {
    if(rth.get.backend() == "tbb")
       stop("this function does not yet work under TBB")
 
-   if (is.data.frame(m)) m <- as.matrix(m)
+   if (is.data.frame(m1)) m1 <- as.matrix(m1)
+   if (is.data.frame(m2)) m2 <- as.matrix(m2)
    .Call("rthpdist", m1, m2, as.integer(nthreads), PACKAGE="Rth")
 }
 
