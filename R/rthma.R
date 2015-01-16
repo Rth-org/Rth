eaac2495221eb13f60202271fd8e4c7bc3d6e789
wrathematics <wrathematics@gmail.com>
2015-01-15 20:19:13 -0500

    moved rthma to C interface

--- R/rthma.R
+++ R/rthma.R
@@ -1,6 +1,11 @@
 rthma <- function(x, w, nthreads=rth.nthreads())
 {
   nthreads <- as.integer(nthreads)
-  .Call("rthma",x,as.integer(w),as.integer(nthreads))
+  ret <- .Call("rthma",x,as.integer(w),as.integer(nthreads))
+  
+  if (is.null(ret))
+    return(0)
+  else
+    return(ret)
 }
 
--- src/rthma.cpp
+++ src/rthma.cpp
@@ -1,4 +1,3 @@
-
 // Rth interface to Thrust moving-average example,
 // simple_moving_average.cu in
 // github.com/thrust/thrust/blob/master/examples/
@@ -11,50 +10,54 @@
 #include <thrust/functional.h>
 #include <thrust/sequence.h>
 
-#include <Rcpp.h>
+#include <R.h>
+#include <Rinternals.h>
+
 #include "backend.h"
 
 // update function, to calculate current moving average value from the
 // previous one
 struct minus_and_divide : public thrust::binary_function<double,double,double>
 {
-    double w;
-    minus_and_divide(double w) : w(w) {}
-    __host__ __device__
-    double operator()(const double& a, const double& b) const
-    { return (a - b) / w; }
+  double w;
+  minus_and_divide(double w) : w(w) {}
+  __host__ __device__
+  double operator()(const double& a, const double& b) const
+  { return (a - b) / w; }
 };
 
+
+
 // computes moving averages from x of window width w
-RcppExport SEXP rthma(SEXP x, SEXP w, SEXP nthreads)
+extern "C" SEXP rthma(SEXP x, SEXP w, SEXP nthreads)
 {
-   Rcpp::NumericVector xa(x);
-   int wa = INTEGER(w)[0];
-   
-   #if RTH_OMP
-   omp_set_num_threads(INT(nthreads));
-   #elif RTH_TBB
-   tbb::task_scheduler_init init(INT(nthreads));
-   #endif
-   
-   // set up device vector and copy xa to it
-   thrust::device_vector<double> dx(xa.begin(), xa.end());
-
-   int xas = xa.size();
-   if (xas < wa)
-       return 0;
-   
-   // allocate device storage for cumulative sums, and compute them
-   thrust::device_vector<double> csums(xa.size() + 1);
-   thrust::exclusive_scan(dx.begin(), dx.end(), csums.begin());
-   // need one more sum at (actually past) the end
-   csums[xas] = xa[xas-1] + csums[xas-1];
-
-   // compute moving averages from cumulative sums
-   Rcpp::NumericVector xb(xas - wa + 1);
-   thrust::transform(csums.begin() + wa, csums.end(), 
-      csums.begin(), xb.begin(), minus_and_divide(double(wa)));
+  SEXP xb;
+  const int xas = LENGTH(x);
+  const int wa = INTEGER(w)[0];
   
-   return xb;
+  #if RTH_OMP
+  omp_set_num_threads(INT(nthreads));
+  #elif RTH_TBB
+  tbb::task_scheduler_init init(INT(nthreads));
+  #endif
+  
+  // set up device vector and copy xa to it
+  thrust::device_vector<double> dx(REAL(x), REAL(x)+xas);
+  if (xas < wa)
+    return R_NilValue;
+  
+  // allocate device storage for cumulative sums, and compute them
+  thrust::device_vector<double> csums(xas + 1);
+  thrust::exclusive_scan(dx.begin(), dx.end(), csums.begin());
+  // need one more sum at (actually past) the end
+  csums[xas] = REAL(x)[xas-1] + csums[xas-1];
+  
+  // compute moving averages from cumulative sums
+  PROTECT(xb = allocVector(REALSXP, xas - wa + 1));
+  thrust::transform(csums.begin() + wa, csums.end(), 
+    csums.begin(), REAL(xb), minus_and_divide(double(wa)));
+  
+  UNPROTECT(1);
+  return xb;
 }
 
