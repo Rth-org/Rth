978f3101a27074af88b455d6b64fb03993aeeb14
wrathematics <wrathematics@gmail.com>
2015-01-15 20:21:44 -0500

    moved rthsort to C interface

--- src/rthsort.cpp
+++ src/rthsort.cpp
@@ -1,18 +1,17 @@
-
-// Rth interface to Thrust sort, Rcpp version
-
 #include <thrust/device_vector.h>
 #include <thrust/sort.h>
 #include <thrust/sort.h>
 
-#include <Rcpp.h>
+#include <R.h>
+#include <Rinternals.h>
+
 #include "backend.h"
 
 
-RcppExport SEXP rthsort_double(SEXP a, SEXP decreasing, SEXP inplace,
-   SEXP nthreads)
+extern "C" SEXP rthsort_double(SEXP a, SEXP decreasing, SEXP inplace,
+  SEXP nthreads)
 {
-  Rcpp::NumericVector xa(a);
+  SEXP b;
   
   #if RTH_OMP
   omp_set_num_threads(INT(nthreads));
@@ -21,27 +20,32 @@ RcppExport SEXP rthsort_double(SEXP a, SEXP decreasing, SEXP inplace,
   #endif
   
   // set up device vector and copy xa to it
-  thrust::device_vector<double> dx(xa.begin(), xa.end());
+  thrust::device_vector<double> dx(REAL(a), REAL(a)+LENGTH(a));
   
   // sort, then copy back to R vector
   if (INTEGER(decreasing)[0])
     thrust::sort(dx.begin(), dx.end(), thrust::greater<double>());
   else
     thrust::sort(dx.begin(), dx.end());
+  
   if (INTEGER(inplace)[0]) {
-     thrust::copy(dx.begin(), dx.end(), xa.begin());
-     // return xa;
-  } else {
-     Rcpp::NumericVector xb(xa.size());
-     thrust::copy(dx.begin(), dx.end(), xb.begin());
-     return xb;
+    thrust::copy(dx.begin(), dx.end(), REAL(a));
+    return R_NilValue;
+  }
+  else
+  {
+    PROTECT(b = allocVector(REALSXP, LENGTH(a)));
+    thrust::copy(dx.begin(), dx.end(), REAL(b));
+    
+    UNPROTECT(1);
+    return b;
   }
 }
 
-RcppExport SEXP rthsort_int(SEXP a, SEXP decreasing, SEXP inplace,
+extern "C" SEXP rthsort_int(SEXP a, SEXP decreasing, SEXP inplace,
    SEXP nthreads)
 {
-  Rcpp::IntegerVector xa(a);
+  SEXP b;
   
   #if RTH_OMP
   omp_set_num_threads(INT(nthreads));
@@ -49,20 +53,25 @@ RcppExport SEXP rthsort_int(SEXP a, SEXP decreasing, SEXP inplace,
   tbb::task_scheduler_init init(INT(nthreads));
   #endif
   
-  thrust::device_vector<int> dx(xa.begin(), xa.end());
+  thrust::device_vector<int> dx(INTEGER(a), INTEGER(a)+LENGTH(a));
   
   if (INTEGER(decreasing)[0])
     thrust::sort(dx.begin(), dx.end(), thrust::greater<int>());
   else
     thrust::sort(dx.begin(), dx.end());
   
-  if (INTEGER(inplace)[0]) {
-     thrust::copy(dx.begin(), dx.end(), xa.begin());
-     // return xa;
-  } else {
-     Rcpp::IntegerVector xb(xa.size());
-     thrust::copy(dx.begin(), dx.end(), xb.begin());
-     return xb;
+  if (INTEGER(inplace)[0])
+  {
+    thrust::copy(dx.begin(), dx.end(), INTEGER(a));
+    return R_NilValue;
+  }
+  else
+  {
+    PROTECT(b = allocVector(INTSXP, LENGTH(a)));
+    thrust::copy(dx.begin(), dx.end(), INTEGER(b));
+    
+    UNPROTECT(1);
+    return b;
   }
 }
 
