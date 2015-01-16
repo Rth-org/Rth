b25a2f0d9bccc859e835cebc6b550290b8fbdacf
wrathematics <wrathematics@gmail.com>
2015-01-15 20:22:46 -0500

    moved rthxposnv to C interface

--- src/rthxposnv.cpp
+++ src/rthxposnv.cpp
@@ -8,7 +8,8 @@
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/iterator/transform_iterator.h>
 
-#include<Rcpp.h>
+#include <R.h>
+#include <Rinternals.h>
 
 // see comments at the top of rthxpos.cpp
 
@@ -16,16 +17,16 @@
 struct transpose_index : public thrust::unary_function<size_t,size_t>
 {
   size_t nr, nc;
-
+  
   __host__ __device__
   transpose_index(size_t _nr, size_t _nc) : nr(_nr), nc(_nc) {}
-
+  
   __host__ __device__
   size_t operator()(size_t linear_index)
   {
       size_t i = linear_index % nr;
       size_t j = linear_index / nr;
-
+      
       return nc * i + j;
   }
 };
@@ -34,10 +35,10 @@ struct transpose_index : public thrust::unary_function<size_t,size_t>
 struct row_index : public thrust::unary_function<size_t,size_t>
 {
   size_t n;
-
+  
   __host__ __device__
   row_index(size_t _n) : n(_n) {}
-
+  
   __host__ __device__
   size_t operator()(size_t i)
   {
@@ -60,15 +61,20 @@ void transpose(size_t nr, size_t nc, thrust::device_vector<T>& src, thrust::devi
 
 RcppExport SEXP rthxposnv(SEXP m) 
 {
-   Rcpp::NumericMatrix tmpm = Rcpp::NumericMatrix(m);
-   int nr = tmpm.nrow();
-   int nc = tmpm.ncol();
-   thrust::device_vector<double> dmat(tmpm.begin(),tmpm.end());
-   // make space for the transpose
-   thrust::device_vector<double> dxp(nr*nc);
-   transpose(nr,nc,dmat,dxp);
-   Rcpp::NumericVector routmat(nc*nr);
-   thrust::copy(dxp.begin(),dxp.end(),routmat.begin());
-   return routmat;
+  SEXP routmat;
+  int nr = nrows(m);
+  int nc = ncols(m);
+  
+  thrust::device_vector<double> dmat(REAL(m), REAL(m)+nr*nc);
+  
+  // make space for the transpose
+  thrust::device_vector<double> dxp(nr*nc);
+  transpose(nr,nc,dmat,dxp);
+  
+  PROTECT(routmat = allocVector(REALSXP, nr*nc));
+  thrust::copy(dxp.begin(), dxp.end(), REAL(routmat));
+  
+  UNPROTECT(1);
+  return routmat;
 }
 
