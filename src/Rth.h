a077037dfe7bf56825112aedcce0e5e4edbba7ab
Drew Schmidt <schmidt@math.utk.edu>
2015-01-15 21:28:02 -0500

    fixed thread getter in cuda case

--- src/Rth.h
+++ src/Rth.h
@@ -18,9 +18,11 @@
 #define INT(x) INTEGER(x)[0]
 
 #if RTH_OMP
-#define RTH_GEN_NTHREADS(nthreads) omp_set_num_threads(INT(nthreads))
+  #define RTH_GEN_NTHREADS(nthreads) omp_set_num_threads(INT(nthreads))
 #elif RTH_TBB
-#define RTH_GEN_NTHREADS(nthreads) tbb::task_scheduler_init init(INT(nthreads))
+  #define RTH_GEN_NTHREADS(nthreads) tbb::task_scheduler_init init(INT(nthreads))
+#else
+  #define RTH_GEN_NTHREADS(nthreads) // nothing
 #endif
 
 #define INT(x) INTEGER(x)[0]
