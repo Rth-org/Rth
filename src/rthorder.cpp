d52bff0d64a1ed6591597fddc0f7f5040bd03ede
wrathematics <wrathematics@gmail.com>
2015-01-15 20:20:21 -0500

    moved rthorder to C interface

--- src/rthorder.cpp
+++ src/rthorder.cpp
@@ -7,7 +7,8 @@
 #include <thrust/sequence.h>
 #include <thrust/sort.h>
 
-#include <Rcpp.h>
+#include <R.h>
+#include <Rinternals.h>
 
 #include "backend.h"
 
@@ -20,35 +21,39 @@ typedef doublevec::iterator doubleveciter;
 // rank(x) is returned
 //
 // x is overwritten
-RcppExport SEXP rthorder(SEXP x, SEXP rnk, SEXP nthreads) 
-{  
-   Rcpp::NumericVector rx(x);
-   const int n = rx.size();
-   bool rank = Rcpp::as<bool>(rnk);
-   
-   Rcpp::IntegerVector ret(n);
-   
-   #if RTH_OMP
-   omp_set_num_threads(INT(nthreads));
-   #elif RTH_TBB
-   tbb::task_scheduler_init init(INT(nthreads));
-   #endif
-   
-   doublevec dx(rx.begin(),rx.end());
-   
-   intvec seq(n);
-   thrust::sequence(seq.begin(),seq.end());
-   thrust::sort_by_key(dx.begin(),dx.end(),seq.begin());
-   if (!rank) thrust::copy(seq.begin(),seq.end(),ret.begin());
-   else {
-      thrust::counting_iterator<int> seqa(0);
-      thrust::counting_iterator<int> seqb =  seqa + n;
-      intvec locout(n);
-      thrust::scatter(seqa,seqb,seq.begin(),locout.begin()); 
-      thrust::copy(locout.begin(),locout.end(),ret.begin());
-   }
-   
-   return ret;
+extern "C" SEXP rthorder(SEXP x, SEXP rnk, SEXP nthreads) 
+{
+  const int n = LENGTH(x);
+  bool rank = (bool) INTEGER(rnk)[0];
+  
+  SEXP ret;
+  PROTECT(ret = allocVector(INTSXP, n));
+  
+  #if RTH_OMP
+  omp_set_num_threads(INT(nthreads));
+  #elif RTH_TBB
+  tbb::task_scheduler_init init(INT(nthreads));
+  #endif
+  
+  
+  doublevec dx(REAL(x), REAL(x)+n);
+  
+  intvec seq(n);
+  thrust::sequence(seq.begin(),seq.end());
+  thrust::sort_by_key(dx.begin(),dx.end(),seq.begin());
+  if (!rank) 
+    thrust::copy(seq.begin(), seq.end(), INTEGER(ret));
+  else 
+  {
+    thrust::counting_iterator<int> seqa(0);
+    thrust::counting_iterator<int> seqb =  seqa + n;
+    intvec locout(n);
+    thrust::scatter(seqa,seqb,seq.begin(),locout.begin()); 
+    thrust::copy(locout.begin(), locout.end(), INTEGER(ret));
+  }
+  
+  UNPROTECT(1);
+  return ret;
 }
 
 
