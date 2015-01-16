7e3397fa5bdcf8cae9962d19d7cf304048fa7616
wrathematics <wrathematics@gmail.com>
2015-01-15 20:20:52 -0500

    moved rthpearson to C interface

--- src/rthpearson.cpp
+++ src/rthpearson.cpp
@@ -1,4 +1,3 @@
-
 // author: N. Matloff
 
 // Rth implementation of Pearson product-moment correlation
@@ -9,53 +8,47 @@
 #include <thrust/inner_product.h>
 #include <math.h>
 
-#include <Rcpp.h>
+#include <R.h>
+#include <Rinternals.h>
+
 #include "backend.h"
 
 typedef thrust::device_vector<int> intvec;
 typedef thrust::device_vector<double> doublevec;
 
-RcppExport SEXP rthpearson(SEXP x, SEXP y, SEXP nthreads)
-{  
-   Rcpp::NumericVector xa(x);
-   Rcpp::NumericVector ya(y);
-   int n = xa.size();
-   doublevec dx(xa.begin(),xa.end());
-   doublevec dy(ya.begin(),ya.end());
-   double zero = (double) 0.0;
-   
-   #if RTH_OMP
-   omp_set_num_threads(INT(nthreads));
-   #elif RTH_TBB
-   tbb::task_scheduler_init init(INT(nthreads));
-   #endif
-   
-   double xy =
-      thrust::inner_product(dx.begin(),dx.end(),dy.begin(),zero);
-   double x2 =
-      thrust::inner_product(dx.begin(),dx.end(),dx.begin(),zero);
-   double y2 =
-      thrust::inner_product(dy.begin(),dy.end(),dy.begin(),zero);
-   double xt =
-      thrust::reduce(dx.begin(),dx.end());
-   double yt =
-      thrust::reduce(dy.begin(),dy.end());
-   double xm = xt/n, ym = yt/n;
-   double xsd = sqrt(x2/n - xm*xm);
-   double ysd = sqrt(y2/n - ym*ym);
-   double cor = (xy/n - xm*ym) / (xsd*ysd);
-   return Rcpp::wrap(cor);
+extern "C" SEXP rthpearson(SEXP x, SEXP y, SEXP nthreads)
+{
+  SEXP cor;
+  int n = LENGTH(x);
+  doublevec dx(REAL(x), REAL(x)+n);
+  doublevec dy(REAL(x), REAL(x)+n);
+  double zero = (double) 0.0;
+  
+  #if RTH_OMP
+  omp_set_num_threads(INT(nthreads));
+  #elif RTH_TBB
+  tbb::task_scheduler_init init(INT(nthreads));
+  #endif
+  
+  double xy =
+    thrust::inner_product(dx.begin(), dx.end(), dy.begin(), zero);
+  double x2 =
+    thrust::inner_product(dx.begin(), dx.end(), dx.begin(), zero);
+  double y2 =
+    thrust::inner_product(dy.begin(), dy.end(), dy.begin(), zero);
+  double xt =
+    thrust::reduce(dx.begin(), dx.end());
+  double yt =
+    thrust::reduce(dy.begin(), dy.end());
+  double xm = xt/n, ym = yt/n;
+  double xsd = sqrt(x2/n - xm*xm);
+  double ysd = sqrt(y2/n - ym*ym);
+  
+  PROTECT(cor = allocVector(REALSXP, 1));
+  REAL(cor)[0] = (xy/n - xm*ym) / (xsd*ysd);
+  
+  UNPROTECT(1);
+  return cor;
 }
-
-// #include <stdio.h>
-// int main(int argc, char **argv)
-// {
-//    double x[6] = {3,5.01,12,6,5,15},
-//            y[6] = {7,8,4.01,10,4,16};
-//    int n = 6;
-//    double rho;
-//    rthpearson(x,y,&n,&rho);
-//    printf("%f\n",rho);  // should be 0.46667
-// }
 
 
