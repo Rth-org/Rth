7c30ef2be6fae93651f4fab1a1dafcdc5ceb9d62
wrathematics <wrathematics@gmail.com>
2015-01-15 20:18:19 -0500

    moved rthkendall to C interface

--- src/rthkendall.cpp
+++ src/rthkendall.cpp
@@ -9,8 +9,10 @@
 
 // see also the method "kendall" in cor.fk(), part of the pcaPP package
 
-#include <Rcpp.h>
 #include <thrust/device_vector.h>
+
+#include <R.h>
+#include <Rinternals.h>
 
 #include "backend.h"
 
@@ -23,35 +25,38 @@
 typedef thrust::device_vector<int> intvec;
 typedef thrust::device_vector<flouble> floublevec;
 
-struct calcgti {  // handle 1 i, all j > i
-   floublevec dx;
-   floublevec dy;
-   int n;
-   flouble *wdx;
-   flouble *wdy;
-   calcgti(floublevec _dx,floublevec _dy,int _n) :
-      dx(_dx),
-      dy(_dy),
-      n(_n)
-      {  wdx = thrust::raw_pointer_cast(&dx[0]);   
-         wdy = thrust::raw_pointer_cast(&dy[0]);  }
-   __device__
-   int operator()(int i)
-   {  flouble xi = wdx[i], yi = wdy[i];
-      int j,count=0;
-      for (j = i+1; j < n; j++)
-         count += ( (xi - wdx[j]) * (yi - wdy[j]) > 0);
-      return count;
-   }
+// handle 1 i, all j > i
+struct calcgti {
+  floublevec dx;
+  floublevec dy;
+  int n;
+  flouble *wdx;
+  flouble *wdy;
+  calcgti(floublevec _dx,floublevec _dy,int _n) :
+    dx(_dx),
+    dy(_dy),
+    n(_n)
+    {
+      wdx = thrust::raw_pointer_cast(&dx[0]);   
+      wdy = thrust::raw_pointer_cast(&dy[0]);
+    }
+  __device__
+  int operator()(int i)
+  {
+    flouble xi = wdx[i], yi = wdy[i];
+    int j,count=0;
+    for (j = i+1; j < n; j++)
+      count += ( (xi - wdx[j]) * (yi - wdy[j]) > 0);
+    return count;
+  }
 };
 
 
 
-RcppExport SEXP rthkendall(SEXP x_, SEXP y_, SEXP nthreads)
+extern "C" SEXP rthkendall(SEXP x, SEXP y, SEXP nthreads)
 {
-  Rcpp::NumericVector x(x_);
-  Rcpp::NumericVector y(y_);
-  Rcpp::NumericVector RET(1);
+  SEXP ret;
+  PROTECT(ret = allocVector(REALSXP, 1));
   const int n = LENGTH(x);
   
   #if RTH_OMP
@@ -62,17 +67,17 @@ RcppExport SEXP rthkendall(SEXP x_, SEXP y_, SEXP nthreads)
   
   thrust::counting_iterator<int> seqa(0);
   thrust::counting_iterator<int> seqb =  seqa + n - 1;
-  floublevec dx(x.begin(), x.end());
-  floublevec dy(y.begin(), y.end());
+  floublevec dx(REAL(x), REAL(x)+n);
+  floublevec dy(REAL(y), REAL(y)+LENGTH(y)); // FIXME this makes me suspicious...
   
   intvec tmp(n-1);
-  thrust::transform(seqa,seqb,tmp.begin(),calcgti(dx,dy,n));
-  int totcount = thrust::reduce(tmp.begin(),tmp.end());
+  thrust::transform(seqa, seqb, tmp.begin(), calcgti(dx,dy,n));
+  int totcount = thrust::reduce(tmp.begin(), tmp.end());
   flouble npairs = n * (n-1) / 2;
   
-  REAL(RET)[0] = (double) (totcount - (npairs-totcount)) / npairs;
+  REAL(ret)[0] = (double) (totcount - (npairs-totcount)) / npairs;
   
-  return RET;
+  return ret;
 }
 
 
